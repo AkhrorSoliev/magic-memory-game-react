725a0ce668c5d9b0517484925e6cc8e1db7c08fa
akhrorsoliev <akhsoliev@gmail.com>
2022-01-25 09:43:44 +0500

    Added a 'matched' Property to Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
 ]
 
 function App() {
@@ -36,7 +36,15 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log('those cars match')
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn()
       } else {
         console.log('those cards do not match')
@@ -50,6 +58,8 @@ function App() {
     setChoiceTwo(null)
     setTurns((prevTurns) => prevTurns + 1)
   }
+
+  console.log(cards)
 
   return (
     <div className="App">
